9e474510e85e409442daef036e700324794709db
Nguyen Ngoc Linh <mail2ngoclinh@gmail.com>
2022-05-29 11:26:15 +0700

    change fetchPolicy

--- src/components/PopularThreads.js
+++ src/components/PopularThreads.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Paper from './ui/Paper';
 import PopularThreadsQuery from './PopularThreadsQuery';
 import ThreadList from './ThreadList';
+import { NUM_POPULAR_THREAD } from './constants';
 
+// We have to set `fetchPolicy` to `network-only` since we don't know
+// which thread user choose to add new posts to. So when user nagivate
+// back to this page, we have to re-fetch it from backend to make sure we
+// get the correct results.
 export default function PopularThreads() {
   return (
     <Paper>
       <PopularThreadsQuery
-        count={10}
+        count={NUM_POPULAR_THREAD}
+        fetchPolicy='network-only'
       >
         {(threads) => (
           <ThreadList threads={threads} />
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,1 +1,3 @@
 export const NUM_POST_PER_PAGE = 10;
+export const NUM_THREAD_PER_PAGE = 10;
+export const NUM_POPULAR_THREAD = 10;
